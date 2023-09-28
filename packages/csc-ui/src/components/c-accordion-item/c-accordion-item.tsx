1631054a01f786fd166d51721eac4c08726f511e
Oskari Väistö <oskari.vaisto@csc.fi>
2023-09-28 12:59:39 +0300

    Working accordion animation

--- packages/csc-ui/src/components/c-accordion-item/c-accordion-item.tsx
+++ packages/csc-ui/src/components/c-accordion-item/c-accordion-item.tsx
@@ -6,6 +6,7 @@ import {
   EventEmitter,
   Listen,
   Element,
+  Host,
 } from '@stencil/core';
 import { mdiChevronRight } from '@mdi/js';
 
@@ -56,11 +57,17 @@ export class CAccordionItem {
 
   private _animation: Animation;
 
-  private _accordionDetails: HTMLDetailsElement;
+  private _accordionElement: HTMLDetailsElement;
 
   private _accordionHeader: HTMLDetailsElement;
 
   private _accordionContent: HTMLDivElement;
+
+  private _isCollapsing = false;
+
+  private _isExpanding = false;
+
+  private static _uniqueId = 0;
 
   @Listen('keydown', { capture: true })
   handleKeyDown(event: KeyboardEvent) {
@@ -69,7 +76,8 @@ export class CAccordionItem {
       (event.key === 'Enter' || event.code === 'Space')
     ) {
       event.preventDefault();
-      this._toggle(event);
+
+      this._onToggleAccordion(event);
     }
   }
 
@@ -78,50 +86,106 @@ export class CAccordionItem {
     event.stopPropagation();
   }
 
-  private _toggle(event: Event) {
-    if (!this.collapsable && this.expanded) {
-      event.preventDefault();
+  private get _collapsedHeight() {
+    const outlineHeight = this.outlined ? 4 : 0;
 
-      return;
+    return this._accordionHeader.offsetHeight + outlineHeight;
+  }
+
+  private get _expandedHeight() {
+    const outlineHeight = this.outlined ? 4 : 0;
+    const headerHeight = this._accordionHeader.offsetHeight;
+    const contentHeight = this._accordionContent.offsetHeight;
+
+    return headerHeight + contentHeight + outlineHeight;
+  }
+
+  private _onToggleAccordion(e) {
+    e.preventDefault();
+
+    if (!this.collapsable && this.expanded) return;
+
+    this._accordionElement.classList.add('c-accordion-item--expanding');
+
+    if (this._isCollapsing || !this.expanded) {
+      this._expand();
+    } else if (this._isExpanding || this.expanded) {
+      this._collapse();
     }
+  }
 
-    this.expanded = !this.expanded;
-    this.itemChange.emit({ value: this.value, expanded: this.expanded });
+  private _collapse() {
+    this._isCollapsing = true;
+
+    const startHeight = `${this._accordionElement.offsetHeight}px`;
+
+    const endHeight = `${this._collapsedHeight}px`;
 
     if (this._animation) {
       this._animation.cancel();
     }
 
-    const detailsHeight = this._accordionDetails.offsetHeight;
+    this.itemChange.emit({ value: this.value, expanded: this.expanded });
 
-    requestAnimationFrame(() => {
-      this._accordionDetails.classList.add('c-accordion-item--expanding');
+    this._animation = this._accordionElement.animate(
+      { height: [startHeight, endHeight] },
+      { duration: 300, easing: 'cubic-bezier(0.25, 0.8, 0.5, 1)' },
+    );
 
-      const headerHeight = this._accordionHeader.offsetHeight;
-      const contentHeight = this._accordionContent.offsetHeight;
+    this._animation.onfinish = () => this._onAnimationFinish(false);
+    this._animation.oncancel = () => (this._isCollapsing = false);
+  }
 
-      this._animation = this._accordionDetails.animate(
-        {
-          height: [`${detailsHeight}px`, `${headerHeight + contentHeight}px`],
-        },
+  private _expand() {
+    this._accordionElement.style.height = `${this._accordionElement.offsetHeight}px`;
+
+    this.expanded = true;
+
+    window.requestAnimationFrame(() => {
+      this._isExpanding = true;
+
+      const startHeight = `${this._collapsedHeight}px`;
+
+      const endHeight = `${this._expandedHeight}px`;
+
+      if (this._animation) {
+        this._animation.cancel();
+      }
+
+      this.itemChange.emit({ value: this.value, expanded: this.expanded });
+
+      this._animation = this._accordionElement.animate(
+        { height: [startHeight, endHeight] },
         { duration: 300, easing: 'cubic-bezier(0.25, 0.8, 0.5, 1)' },
       );
 
-      this._animation.onfinish = () => {
-        this._animation = null;
-        this._accordionDetails.classList.remove('c-accordion-item--expanding');
-      };
+      this._animation.onfinish = () => this._onAnimationFinish(true);
+      this._animation.oncancel = () => (this._isExpanding = false);
     });
   }
 
-  private static _uniqueId = 0;
+  private _onAnimationFinish(isOpen: boolean) {
+    this.expanded = isOpen;
+
+    this._animation = null;
+
+    this._isCollapsing = false;
+
+    this._isExpanding = false;
+
+    this._accordionElement.style.height = '';
+
+    this._accordionElement.classList.remove('c-accordion-item--expanding');
+
+    this.expanded = isOpen;
+  }
 
   componentWillLoad() {
     CAccordionItem._uniqueId += 1;
   }
 
   render() {
-    const hostClasses = {
+    const accordionClasses = {
       'c-accordion-item': true,
       'c-accordion-item--expanded': this.expanded,
       'c-accordion-item--outlined': this.outlined,
@@ -129,6 +193,7 @@ export class CAccordionItem {
 
     const headerClasses = {
       'c-accordion-item__header': true,
+      'c-accordion-item__header--collapsable': this.collapsable,
       'c-accordion-item__header--expanded': this.expanded,
     };
 
@@ -138,49 +203,47 @@ export class CAccordionItem {
     };
 
     return (
-      <details
-        id={`panel__${CAccordionItem._uniqueId}`}
-        ref={(el) => (this._accordionDetails = el as HTMLDetailsElement)}
-        class={hostClasses}
-        open={!!this.expanded}
-      >
-        <summary
-          id={`header__${CAccordionItem._uniqueId}`}
-          ref={(el) => (this._accordionHeader = el as HTMLDetailsElement)}
-          aria-controls={`panel__${CAccordionItem._uniqueId}`}
-          aria-expanded={this.expanded.toString()}
-          aria-label={this.heading}
-          class={headerClasses}
-          onClick={(event) => this._toggle(event)}
+      <Host>
+        <details
+          id={`panel__${CAccordionItem._uniqueId}`}
+          ref={(el) => (this._accordionElement = el as HTMLDetailsElement)}
+          class={accordionClasses}
+          open={!!this.expanded}
         >
-          <div class="c-accordion-item__icon" aria-visible="hidden">
-            <slot name="icon"></slot>
-          </div>
-
-          <div class="c-accordion-item__title">{this.heading}</div>
-
-          <div class={indicatorClasses}>
-            <c-icon
-              path={mdiChevronRight}
-              color="var(--c-accordion-item-text)"
-              class={this.expanded && 'expanded'}
-            ></c-icon>
-          </div>
-        </summary>
-
-        <div
-          ref={(el) => (this._accordionContent = el as HTMLDivElement)}
-          class="c-accordion-item__content-wrapper"
-        >
-          <div
-            class="c-accordion-item__content"
-            role="region"
-            aria-labelledby={`header__${CAccordionItem._uniqueId}`}
+          <summary
+            ref={(el) => (this._accordionHeader = el as HTMLDetailsElement)}
+            class={headerClasses}
+            onClick={(event) => this._onToggleAccordion(event)}
           >
-            {this.expanded && <slot></slot>}
+            <div class="c-accordion-item__icon" aria-visible="hidden">
+              <slot name="icon"></slot>
+            </div>
+
+            <div class="c-accordion-item__title">{this.heading}</div>
+
+            <div class={indicatorClasses}>
+              <c-icon
+                path={mdiChevronRight}
+                color="var(--c-accordion-item-text)"
+                class={{ expanded: this.expanded }}
+              ></c-icon>
+            </div>
+          </summary>
+
+          <div
+            ref={(el) => (this._accordionContent = el as HTMLDivElement)}
+            class="c-accordion-item__content-wrapper"
+          >
+            <div
+              class="c-accordion-item__content"
+              role="region"
+              aria-labelledby={`header__${CAccordionItem._uniqueId}`}
+            >
+              <slot></slot>
+            </div>
           </div>
-        </div>
-      </details>
+        </details>
+      </Host>
     );
   }
 }
