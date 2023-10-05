5b5a3ef0ffd44ffc80749564b92ed22963c6741e
Oskari Väistö <oskari.vaisto@csc.fi>
2023-10-05 16:15:09 +0300

    Use component data types from csc-ui package

--- packages/csc-ui-documentation/types/docs.d.ts
+++ packages/csc-ui-documentation/types/docs.d.ts
@@ -1,37 +1,5 @@
-export interface ComponentData {
-  name?: string;
-  filePath: string;
-  encapsulation: string;
-  tag: string;
-  readme: string;
-  docs: string;
-  docsTags: any[];
-  usage: any;
-  props: any[];
-  methods: any[];
-  events: EventItem[];
-  listeners: Listener[];
-  styles: any[];
-  slots: any[];
-  parts: any[];
-  dependents: string[];
-  dependencies: string[];
-  dependencyGraph: { [key: string]: string[] };
-  children?: ComponentData[];
-}
+import { JsonDocsComponent } from '../../csc-ui/docs.d';
 
-export interface Listener {
-  event: string;
-  capture: boolean;
-  passive: boolean;
-}
-
-export interface EventItem {
-  event: string;
-  detail: any;
-  bubbles: boolean;
-  cancelable: boolean;
-  composed: boolean;
-  docs: string;
-  docsTags: any[];
-}
+export type ComponentData = JsonDocsComponent & {
+  name: string;
+};
