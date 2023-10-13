5f088d69684dd97e7ad488ec44432fd42775057a
Oskari Väistö <oskari.vaisto@csc.fi>
2023-10-13 09:31:34 +0300

    feat: New component - c-list

---
+++ packages/csc-ui/src/components/c-list/c-list.tsx
@@ -0,0 +1,45 @@
+import { Component, Element, Host, Prop, Watch, h } from '@stencil/core';
+
+@Component({
+  tag: 'c-list',
+  styleUrl: 'c-list.scss',
+})
+export class CList {
+  @Element() el!: HTMLCListElement;
+
+  /**
+   * Disable the list
+   */
+  @Prop() disabled = false;
+
+  @Watch('disabled')
+  onDisabledChange(disabled) {
+    this._items.forEach((item) => {
+      const isDisabled =
+        item.hasAttribute('disabled') && !('disabled' in item.dataset);
+
+      // ignore especially disabled items
+      if (!isDisabled) {
+        item.disabled = !!disabled;
+        item.disabledByParent = !!disabled;
+      }
+    });
+  }
+
+  private get _items() {
+    return Array.from(this.el.querySelectorAll('c-list-item'));
+  }
+
+  componentDidLoad() {
+    this.onDisabledChange(this.disabled);
+  }
+
+  render() {
+    const classes = {
+      'c-list': true,
+      'c-list--disabled': this.disabled,
+    };
+
+    return <Host role="list" class={classes}></Host>;
+  }
+}
