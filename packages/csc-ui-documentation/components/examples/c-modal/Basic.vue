704076e75b6102d356c4c25a7cf280714b82e598
Ville Eriksson <ville.eriksson@csc.fi>
2023-10-19 10:53:30 +0300

    Documentation: Modal

---
+++ packages/csc-ui-documentation/components/examples/c-modal/Basic.vue
@@ -0,0 +1,42 @@
+<template>
+  <component-example name="basic">
+    <template #title>Basic usage</template>
+
+    <c-button
+      @click="basicModal = true"
+      @keyup.enter="basicModal = true"
+      @keyup.space="basicModal = true"
+    >
+      Open modal
+    </c-button>
+
+    <c-modal v-model="basicModal" v-control>
+      <c-card>
+        <c-card-title>Modal</c-card-title>
+
+        <c-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </c-card-content>
+
+        <c-card-actions>
+          <c-button
+            @click="basicModal = false"
+            @keyup.enter="basicModal = false"
+            @keyup.space="basicModal = false"
+          >
+            Close
+          </c-button>
+        </c-card-actions>
+      </c-card>
+    </c-modal>
+  </component-example>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const basicModal = ref(false);
+</script>
---
+++ packages/csc-ui-documentation/components/examples/c-modal/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <basic />
+
+  <custom-width />
+
+  <dismissable />
+</template>
+
+<script setup lang="ts">
+import Basic from './Basic.vue';
+import CustomWidth from './CustomWidth.vue';
+import Dismissable from './Dismissable.vue';
+</script>
