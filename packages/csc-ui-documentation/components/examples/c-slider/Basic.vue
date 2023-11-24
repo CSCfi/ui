84114547821d9ffc35ebb5358a2fa0c07690eef8
Oskari Väistö <oskari.vaisto@csc.fi>
2023-11-24 13:35:33 +0200

    feat: c-slider: New component

---
+++ packages/csc-ui-documentation/components/examples/c-slider/Basic.vue
@@ -0,0 +1,51 @@
+<template>
+  <component-example rows name="basic">
+    <template #title>Basic usage</template>
+
+    <c-switch v-model="showTicks" v-control>Ticks</c-switch>
+
+    <c-switch v-model="showLabels" v-control>Labels</c-switch>
+
+    <c-switch v-model="showTooltip" v-control>Tooltip</c-switch>
+
+    <c-switch v-model="isDisabled" v-control>Disabled</c-switch>
+
+    <c-radio-group v-model="segments" v-control label="Segment count" inline>
+      <c-radio value="2">2 (every 50%)</c-radio>
+
+      <c-radio value="4">4 (every 25%)</c-radio>
+
+      <c-radio value="5">5 (every 20%)</c-radio>
+
+      <c-radio value="10" checked>10 (every 10%)</c-radio>
+    </c-radio-group>
+
+    <c-slider
+      v-model="sliderValue"
+      v-control
+      label="Monthly usage"
+      class="flex-1"
+      :show-labels="showLabels"
+      :disable-tooltip="!showTooltip"
+      :disabled="isDisabled"
+      :ticks="showTicks"
+      :segments="segments"
+    />
+  </component-example>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const sliderValue = ref(40);
+
+const showLabels = ref(true);
+
+const showTicks = ref(true);
+
+const showTooltip = ref(true);
+
+const isDisabled = ref(false);
+
+const segments = ref('10');
+</script>
---
+++ packages/csc-ui-documentation/components/examples/c-slider/Volume.vue
@@ -0,0 +1,36 @@
+<template>
+  <component-example rows name="volume">
+    <template #title>Usage without a label</template>
+
+    <c-row gap="8" align="center">
+      <c-icon-button text :disabled="volume === 0" @click="onVolumeDecrease()">
+        <c-icon :path="mdiVolumeLow" />
+      </c-icon-button>
+
+      <c-slider v-model="volume" v-control aria-label="Volume" segments="5" />
+
+      <c-icon-button
+        text
+        :disabled="volume === 100"
+        @click="onVolumeIncrease()"
+      >
+        <c-icon :path="mdiVolumeHigh" />
+      </c-icon-button>
+    </c-row>
+  </component-example>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { mdiVolumeHigh, mdiVolumeLow } from '@mdi/js';
+
+const volume = ref(80);
+
+const onVolumeDecrease = () => {
+  volume.value = Math.max(0, volume.value - 10);
+};
+
+const onVolumeIncrease = () => {
+  volume.value = Math.min(100, volume.value + 10);
+};
+</script>
