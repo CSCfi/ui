b8ace1a093fc1ca351a4cb657e7fd431e81813c0
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-12-15 15:28:14 +0200

    c-switch updated visual tests

--- packages/csc-ui/src/components/c-switch/test/c-switch.visual.ts
+++ packages/csc-ui/src/components/c-switch/test/c-switch.visual.ts
@@ -1,41 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { takeScreenshots } from '../../../utils/test/takeScreenshot';
 
 test.beforeEach(async ({ page }, testInfo) => {
-  await page.goto('http://localhost:3000/c-switch');
+  await page.goto('http://localhost:3000/components/c-switch');
 
   testInfo.snapshotSuffix = '';
 });
 
-test('Default', async ({ page }) => {
-  const button = page.locator('app-example[name="inactive"] c-switch');
-
-  await expect(button).toHaveScreenshot();
-
-  await button.locator('label span').click();
-
-  await expect(button).toHaveScreenshot();
+test('Basic', async ({ page }) => {
+  await takeScreenshots(page, 'basic', 'c-switch');
 });
 
-test('Active', async ({ page }) => {
-  const button = page.locator('app-example[name="active"] c-switch');
-
-  await expect(button).toHaveScreenshot();
-});
-
-test('Disabled', async ({ page }) => {
-  const button = page.locator('app-example[name="disabled"] c-switch');
-
-  await expect(button).toHaveScreenshot();
-});
-
-test('Label', async ({ page }) => {
-  const button = page.locator('app-example[name="labeled"] c-switch');
-
-  await expect(button).toHaveScreenshot();
-});
-
-test('Required', async ({ page }) => {
-  const button = page.locator('app-example[name="required"] c-switch');
-
-  await expect(button).toHaveScreenshot();
+test('Index', async ({ page }) => {
+  await takeScreenshots(page, 'index', 'c-switch');
 });
