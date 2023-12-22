8739cb1bab8f3642db268d73d5684799cc2c21d0
Kata Sara-aho <kata.sara-aho@csc.fi>
2023-12-22 14:44:49 +0200

    unused method removal

--- packages/csc-ui/src/components/c-card/test/c-card.visual.ts
+++ packages/csc-ui/src/components/c-card/test/c-card.visual.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { takeScreenshots } from '../../../utils/test/takeScreenshot';
 
 test.beforeEach(async ({ page }, testInfo) => {
