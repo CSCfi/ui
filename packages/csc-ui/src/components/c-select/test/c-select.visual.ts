38948469d130bb8d64835d52f47f16ef704aa268
Kata Sara-aho <kata.sara-aho@csc.fi>
2024-01-03 15:32:40 +0200

    c-select updated visual tests

--- packages/csc-ui/src/components/c-select/test/c-select.visual.ts
+++ packages/csc-ui/src/components/c-select/test/c-select.visual.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { takeScreenshots } from '../../../utils/test/takeScreenshot';
 
 test.beforeEach(async ({ page }, testInfo) => {
@@ -8,6 +8,16 @@ test.beforeEach(async ({ page }, testInfo) => {
 });
 
 test('Basic', async ({ page }) => {
+  await takeScreenshots(page, 'basic', 'c-select');
+
+  await page.locator('c-select').locator('c-icon-button').first().click();
+
+  const list = page.locator('c-select').locator('dialog').first();
+
+  await expect(list).toHaveScreenshot();
+
+  await page.locator('c-select').locator('ul li').nth(1).click();
+
   await takeScreenshots(page, 'basic', 'c-select');
 });
 
