f1e92a754f124f5fcd762c5038f293dc86bacfad
Kata Sara-aho <kata.sara-aho@csc.fi>
2024-01-03 15:01:58 +0200

    c-pagination updated visual tests

--- packages/csc-ui/src/components/c-pagination/test/c-pagination.visual.ts
+++ packages/csc-ui/src/components/c-pagination/test/c-pagination.visual.ts
@@ -1,63 +1,27 @@
 import { test, expect } from '@playwright/test';
+import { takeScreenshots } from '../../../utils/test/takeScreenshot';
 
 test.beforeEach(async ({ page }, testInfo) => {
-  await page.goto('http://localhost:3000/c-pagination');
+  await page.goto('http://localhost:3000/components/c-pagination');
 
   testInfo.snapshotSuffix = '';
 });
 
-test('Default', async ({ page }) => {
-  const paginationWrapper = page
-    .locator('app-example[name="basic"] div')
-    .nth(1);
+test.only('Basic', async ({ page }) => {
+  const pagination = page.locator('div').filter({ hasText: 'Default' }).first();
 
-  await expect(paginationWrapper).toHaveScreenshot();
+  await expect(pagination).toHaveScreenshot();
 
-  await paginationWrapper.getByRole('button', { name: 'page 2 of 10' }).click();
-
-  await expect(paginationWrapper).toHaveScreenshot();
-
-  await paginationWrapper.getByRole('button', { name: 'Next page' }).click();
-
-  await expect(paginationWrapper).toHaveScreenshot();
-
-  await paginationWrapper
-    .getByRole('button', { name: 'page 10 of 10' })
+  await page
+    .locator('c-pagination')
+    .locator('c-icon-button')
+    .filter({ hasText: '2' })
+    .first()
     .click();
 
-  await expect(paginationWrapper).toHaveScreenshot();
-
-  await paginationWrapper
-    .getByRole('button', { name: 'Previous page' })
-    .click();
-
-  await expect(paginationWrapper).toHaveScreenshot();
+  await expect(pagination).toHaveScreenshot();
 });
 
 test('Simple', async ({ page }) => {
-  const paginationWrapper = page
-    .locator('app-example[name="simple"] div')
-    .nth(1);
-
-  await expect(paginationWrapper).toHaveScreenshot();
-
-  await paginationWrapper.getByRole('button', { name: 'Next page' }).click();
-
-  await expect(paginationWrapper).toHaveScreenshot();
-});
-
-test('Programmatical', async ({ page }) => {
-  const paginationWrapper = page
-    .locator('app-example[name="programmatical"] div')
-    .nth(1);
-
-  await expect(paginationWrapper).toHaveScreenshot();
-
-  await page.getByRole('button', { name: 'Page 2', exact: true }).click();
-
-  await expect(paginationWrapper).toHaveScreenshot();
-
-  await page.getByRole('button', { name: '50 items per page' }).click();
-
-  await expect(paginationWrapper).toHaveScreenshot();
+  await takeScreenshots(page, 'simple', 'c-pagination');
 });
