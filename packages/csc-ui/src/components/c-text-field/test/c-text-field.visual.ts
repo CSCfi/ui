f0f934fd1e02e69b95d719be4deda33c7e1f7c40
Kata Sara-aho <kata.sara-aho@csc.fi>
2024-01-04 13:24:51 +0200

    c-text-field visual tests update

--- packages/csc-ui/src/components/c-text-field/test/c-text-field.visual.ts
+++ packages/csc-ui/src/components/c-text-field/test/c-text-field.visual.ts
@@ -1,65 +1,113 @@
 import { test, expect } from '@playwright/test';
+import { takeScreenshots } from '../../../utils/test/takeScreenshot';
 
 test.beforeEach(async ({ page }, testInfo) => {
-  await page.goto('http://localhost:3000/c-text-field');
-
-  await page.waitForTimeout(500);
+  await page.goto('http://localhost:3000/components/c-text-field');
 
   testInfo.snapshotSuffix = '';
 });
 
+test('Basic', async ({ page }) => {
+  await takeScreenshots(page, 'basic', 'c-text-field');
+
+  const textField = page.locator('[data-test="basic"] c-text-field').first();
+
+  await textField.getByLabel('Text field', { exact: true }).fill('Example');
+
+  await page.waitForTimeout(1000);
+
+  await takeScreenshots(page, 'basic', 'c-text-field');
+});
+
 test('Date', async ({ page }) => {
-  const textField = page.locator('app-example[name="date"] div').nth(1);
+  await takeScreenshots(page, 'date', 'c-text-field');
 
-  await expect(textField).toHaveScreenshot();
+  const textField = page.locator('[data-test="date"] c-text-field').first();
 
-  await textField.getByLabel('Date', { exact: true }).fill('2023-09-05');
+  await textField.getByLabel('Date field', { exact: true }).fill('2024-01-01');
 
-  await page.waitForTimeout(350);
+  await page.waitForTimeout(1000);
 
-  await expect(textField).toHaveScreenshot();
+  await takeScreenshots(page, 'date', 'c-text-field');
 });
 
 test('Password', async ({ page }) => {
-  const textField = page.locator('app-example[name="password"] div').nth(1);
+  await takeScreenshots(page, 'password', 'c-text-field');
 
-  await expect(textField).toHaveScreenshot();
+  const textField = page.locator('[data-test="password"] c-text-field').first();
 
-  await textField.getByLabel('Password', { exact: true }).fill('salasana');
+  await textField
+    .getByLabel('Password field', { exact: true })
+    .fill('Example123');
 
   await page.waitForTimeout(350);
 
-  await expect(textField).toHaveScreenshot();
-});
+  await takeScreenshots(page, 'password', 'c-text-field');
 
-test('Disabled', async ({ page }) => {
-  const textField = page.locator('app-example[name="disabled"] div').nth(1);
+  await page
+    .locator('[data-test="password"] c-text-field')
+    .locator('svg')
+    .first()
+    .click();
 
-  await expect(textField).toHaveScreenshot();
+  await takeScreenshots(page, 'password', 'c-text-field');
 });
 
 test('Shadow', async ({ page }) => {
-  const textField = page.locator('app-example[name="shadow"] div').nth(1);
+  await takeScreenshots(page, 'shadow', 'c-text-field');
+
+  const textField = page.locator('[data-test="shadow"] c-text-field').first();
+
+  await textField.getByTitle('Shadow field').fill('Example');
+
+  await page.waitForTimeout(350);
+
+  await takeScreenshots(page, 'shadow', 'c-text-field');
+});
+
+test('Slots', async ({ page }) => {
+  await takeScreenshots(page, 'slots', 'c-text-field');
+
+  const textField = page.locator('[data-test="slots"] c-text-field').nth(1);
+
+  await textField.getByLabel('Clearable', { exact: true }).fill('Example');
+
+  await page.waitForTimeout(350);
 
   await expect(textField).toHaveScreenshot();
 
-  await textField
-    .getByRole('textbox', { name: 'Text field with a shadow' })
-    .fill('Testi');
+  await page
+    .locator('[data-test="slots"] c-text-field')
+    .locator('c-icon-button')
+    .first()
+    .click();
 
   await page.waitForTimeout(350);
 
   await expect(textField).toHaveScreenshot();
 });
 
+test('Status', async ({ page }) => {
+  await takeScreenshots(page, 'status', 'c-text-field');
+
+  await page
+    .locator('c-switch')
+    .filter({ hasText: 'Toggle' })
+    .locator('span')
+    .first()
+    .click();
+
+  await takeScreenshots(page, 'status', 'c-text-field');
+});
+
 test('Textarea', async ({ page }) => {
-  const textField = page.locator('app-example[name="textarea"] div').nth(1);
+  await takeScreenshots(page, 'textarea', 'c-text-field');
 
-  await expect(textField).toHaveScreenshot();
+  const textField = page.locator('[data-test="textarea"] c-text-field').first();
 
-  await textField.getByLabel('I am a textarea', { exact: true }).fill('Testi');
+  await textField
+    .getByLabel('I am a textarea', { exact: true })
+    .fill('Example');
 
-  await page.waitForTimeout(350);
-
-  await expect(textField).toHaveScreenshot();
+  await takeScreenshots(page, 'textarea', 'c-text-field');
 });
