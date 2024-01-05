5c16bb918e8e5c684bf2b07f24e663e728e5949c
Kata Sara-aho <kata.sara-aho@csc.fi>
2024-01-05 15:32:12 +0200

    in progress

--- packages/csc-ui/src/components/c-toasts/test/c-toasts.visual.ts
+++ packages/csc-ui/src/components/c-toasts/test/c-toasts.visual.ts
@@ -1,47 +1,49 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { takeScreenshots } from '../../../utils/test/takeScreenshot';
 
 test.beforeEach(async ({ page }, testInfo) => {
-  await page.goto('http://localhost:3000/c-toasts');
+  await page.goto('http://localhost:3000/components/c-toasts');
 
   testInfo.snapshotSuffix = '';
 });
 
-test('Default', async ({ page }) => {
+test('Basic', async ({ page }) => {
   const toastsWrapper = page.locator('app-example[name="basic"] div').nth(1);
 
   await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
 
-  const toasts = toastsWrapper.locator('c-toasts');
-
-  await expect(toasts).toHaveScreenshot();
-
-  await toasts.locator('c-icon-button').click();
-
-  await toastsWrapper.locator('.c-input-menu__input').first().click();
-
-  await toastsWrapper.getByRole('option', { name: 'Success' }).click();
-
-  await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
-
-  await expect(toasts).toHaveScreenshot();
-
-  await toasts.locator('c-icon-button').click();
-
-  await toastsWrapper.locator('.c-input-menu__input').first().click();
-
-  await toastsWrapper.getByRole('option', { name: 'Warning' }).click();
-
-  await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
-
-  await expect(toasts).toHaveScreenshot();
-
-  await toasts.locator('c-icon-button').click();
-
-  await toastsWrapper.locator('.c-input-menu__input').first().click();
-
-  await toastsWrapper.getByRole('option', { name: 'Error' }).click();
-
-  await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
-
-  await expect(toasts).toHaveScreenshot();
+  await takeScreenshots(page, 'basic', 'c-toasts');
 });
+
+// test('Default', async ({ page }) => {
+
+//   await toasts.locator('c-icon-button').click();
+
+//   await toastsWrapper.locator('.c-input-menu__input').first().click();
+
+//   await toastsWrapper.getByRole('option', { name: 'Success' }).click();
+
+//   await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
+
+//   await expect(toasts).toHaveScreenshot();
+
+//   await toasts.locator('c-icon-button').click();
+
+//   await toastsWrapper.locator('.c-input-menu__input').first().click();
+
+//   await toastsWrapper.getByRole('option', { name: 'Warning' }).click();
+
+//   await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
+
+//   await expect(toasts).toHaveScreenshot();
+
+//   await toasts.locator('c-icon-button').click();
+
+//   await toastsWrapper.locator('.c-input-menu__input').first().click();
+
+//   await toastsWrapper.getByRole('option', { name: 'Error' }).click();
+
+//   await toastsWrapper.getByRole('button', { name: 'Add toast' }).click();
+
+//   await expect(toasts).toHaveScreenshot();
+// });
