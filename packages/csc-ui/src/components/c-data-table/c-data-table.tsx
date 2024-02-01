89f74343acb00476827a40fc65e002cd4cf0a9da
Ville Eriksson <ville.eriksson@csc.fi>
2024-02-01 14:00:37 +0200

    feat(c-pagination): Renamed and moved page sizes to options object
    BREAKING CHANGE: Renamed and moved page sizes to options object. PageSizes should be defined in pagination object instead of the footerOptions object in c-data-table.

--- packages/csc-ui/src/components/c-data-table/c-data-table.tsx
+++ packages/csc-ui/src/components/c-data-table/c-data-table.tsx
@@ -73,7 +73,6 @@ export class CDataTable {
    * Items per page options
    */
   @Prop() footerOptions: CDataTableFooterOptions = {
-    itemsPerPageOptions: [5, 25, 50, 100],
     hideDetails: false,
     simple: false,
     hideRange: false,
