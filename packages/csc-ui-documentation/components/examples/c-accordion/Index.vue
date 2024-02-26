7e9f4f62648f02acfb3bb2f958d1008df86a6674
Oskari Väistö <oskari.vaisto@csc.fi>
2024-02-26 11:39:36 +0200

    docs: add an example for the c-accordion header slot usage

--- packages/csc-ui-documentation/components/examples/c-accordion/Index.vue
+++ packages/csc-ui-documentation/components/examples/c-accordion/Index.vue
@@ -6,6 +6,8 @@
   <multiple />
 
   <outlined />
+
+  <custom-header />
 </template>
 
 <script setup lang="ts">
@@ -13,6 +15,7 @@ import Basic from './Basic.vue';
 import Mandatory from './Mandatory.vue';
 import Multiple from './Multiple.vue';
 import Outlined from './Outlined.vue';
+import CustomHeader from './CustomHeader.vue';
 
 defineOptions({
   inheritAttrs: false,
