e4673986aa15b46814500b97889ebc3f15cc35bd
Oskari Väistö <oskari.vaisto@csc.fi>
2024-09-20 10:59:21 +0300

    feat(c-card-title): Allow placing actions at end of c-title

--- packages/csc-ui-documentation/components/examples/c-card/Index.vue
+++ packages/csc-ui-documentation/components/examples/c-card/Index.vue
@@ -1,12 +1,15 @@
 <template>
   <basic />
 
+  <title-actions />
+
   <fullscreen />
 </template>
 
 <script setup lang="ts">
 import Basic from './Basic.vue';
 import Fullscreen from './Fullscreen.vue';
+import TitleActions from './TitleActions.vue';
 
 defineOptions({
   inheritAttrs: false,
--- packages/csc-ui/src/components/c-card-title/c-card-title.tsx
+++ packages/csc-ui/src/components/c-card-title/c-card-title.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, Element, h } from '@stencil/core';
 /**
  * @parent c-card
  * @slot Default slot - Card title text
@@ -9,11 +9,78 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class CCardTitle {
+  @Element() hostElement: HTMLCCardTitleElement;
+
+  private _slotHasContent = false;
+
+  private _isWrapped = false;
+
+  private _resizeObserver: ResizeObserver;
+
+  private _wrapperElement: HTMLHeadingElement;
+
+  private _headerElement: HTMLDivElement;
+
+  private _actionsElement: HTMLDivElement;
+
+  private _handleResize() {
+    if (!this._headerElement || !this._actionsElement) return;
+
+    this._isWrapped =
+      this._headerElement.getBoundingClientRect().top <
+      this._actionsElement.getBoundingClientRect().top;
+
+    this._wrapperElement.classList.toggle(
+      'c-card-title--wrapped',
+      this._isWrapped,
+    );
+  }
+
+  componentWillLoad() {
+    this._slotHasContent = !!this.hostElement.querySelector('[slot="actions"]');
+  }
+
+  componentDidLoad() {
+    this._resizeObserver = new ResizeObserver(() => {
+      this._handleResize();
+    });
+
+    this._resizeObserver.observe(this.hostElement);
+  }
+
+  disconnectedCallback() {
+    this._resizeObserver?.disconnect();
+  }
+
   render() {
+    const classes = {
+      'c-card-title': true,
+      'c-card-title--actions': this._slotHasContent,
+    };
+
     return (
-      <header class="c-card-title">
-        <slot></slot>
-        <div class="c-card-title__underline"></div>
+      <header
+        class={classes}
+        ref={(el) => (this._wrapperElement = el as HTMLHeadingElement)}
+      >
+        <div
+          class="c-card-title__header"
+          ref={(el) => (this._headerElement = el as HTMLDivElement)}
+        >
+          <p>
+            <slot></slot>
+          </p>
+          <div class="c-card-title__underline"></div>
+        </div>
+
+        {this._slotHasContent && (
+          <div
+            class="c-card-title__actions"
+            ref={(el) => (this._actionsElement = el as HTMLDivElement)}
+          >
+            <slot name="actions"></slot>
+          </div>
+        )}
       </header>
     );
   }
