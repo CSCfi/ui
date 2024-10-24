5b52cdef46f87744582cb9d19ea4d15722717530
Ville Eriksson <ville.eriksson@csc.fi>
2024-10-24 10:54:48 +0300

    fix(Types): Workaround to include missing types

---
+++ packages/csc-ui/src/additional-types.d.ts
@@ -0,0 +1,14 @@
+export * from './components';
+
+export {
+  CToastType,
+  CToastPosition,
+  CPaginationTextOverrides,
+  CDataTableDataItem,
+  CDataTableChild,
+  CDataTableComponent,
+  CDataTableComponentParams,
+  CDataTableFunctionParams,
+  CNotificationItem,
+  CMenuCustomTrigger,
+} from './types';
