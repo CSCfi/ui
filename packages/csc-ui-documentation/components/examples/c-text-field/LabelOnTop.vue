9eed29668c61359ee37bace9eed9115ba6a67cfc
Oskari Väistö <oskari.vaisto@csc.fi>
2025-06-12 15:50:48 +0300

    fix(c-text-field): Fix label on top color customization

--- packages/csc-ui-documentation/components/examples/c-text-field/LabelOnTop.vue
+++ packages/csc-ui-documentation/components/examples/c-text-field/LabelOnTop.vue
@@ -2,7 +2,11 @@
   <component-example name="label-on-top" rows>
     <template #title>Label on top</template>
 
-    <c-text-field label="Text field" label-on-top />
+    <c-text-field
+      label="Text field"
+      hint="I have a label on top"
+      label-on-top
+    />
 
     <c-text-field label="Text area" rows="6" label-on-top />
   </component-example>
